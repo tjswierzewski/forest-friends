8742d64d443a4b23849844e6a678731bab8c89dd
Tim Swierzewski <TJswierzewski@gmail.com>
2022-02-05 18:28:40 -0500

    Add info content

--- pages/about.js
+++ pages/about.js
@@ -4,43 +4,33 @@ export default function About() {
   return (
     <Container maxWidth="xl" sx={{ margin: '0 auto 2rem auto' }}>
       <Typography variant="h2">Our Mission</Typography>
-      <Typography variant="body1" sx={{ margin: '0 auto' }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet dapibus diam, a tempus
-        odio. Quisque ultricies dictum metus id iaculis. Aliquam rutrum ante elit, id faucibus erat
-        tincidunt non. Suspendisse rhoncus enim vel nulla laoreet vehicula. Mauris imperdiet rutrum
-        lectus non accumsan. Aliquam malesuada neque a aliquam scelerisque. Nam accumsan, felis non
-        venenatis dictum, sapien ex vestibulum nibh, quis auctor dolor nisl et dolor. Cras augue
-        mauris, ultricies eget pulvinar ac, fringilla vel quam. Praesent neque ex, viverra a dolor
-        non, faucibus porta leo. Nam quis vehicula lectus. Nulla erat augue, ornare at condimentum sit
-        amet, imperdiet quis augue. Morbi dictum porta mattis. Aliquam vitae orci in massa gravida
-        dapibus ut in purus. Mauris eu elit vel nisl lacinia mattis a eget arcu.
+      <Typography variant="body1" sx={{ margin: '0 auto', maxWidth: '80%' }}>
+        Forest Friends is a new and innovative animal welfare initiative started by a group of 5
+        students at Northeastern University in Boston, Massachusetts. Our goal consists in tackling
+        the restoration of endangered animal species by implementing a technologically advanced way of
+        making long-distance adoption fun and rewarding. Through the use of Blockchain technology,
+        we’re able to offer our users the possibility to support researchers and wildlife associations
+        while remaining in close contact with the adopted animal.
       </Typography>
       <Typography variant="h2">How It Works</Typography>
-      <Typography variant="body1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ornare lorem at laoreet
-        porttitor. Vivamus et nulla at sapien finibus vehicula sit amet sed nisi. Fusce consequat eros
-        at nisi ornare, quis ultricies mi ullamcorper. Aenean consequat volutpat diam eget vestibulum.
-        Donec eget elit mauris. Morbi dictum pellentesque turpis eu feugiat. Nunc eleifend nisl sed
-        erat lacinia, at congue ligula viverra. Aenean non ligula sit amet odio varius consequat.
-        Nullam egestas ante id aliquam venenatis. Aenean ac neque leo. Maecenas volutpat urna eros,
-        nec mattis ligula scelerisque sit amet. Duis nec posuere purus, et faucibus sapien. Aenean sed
-        mauris elit.
+      <Typography variant="body1" sx={{ margin: '0 auto', maxWidth: '80%' }}>
+        When you buy one of our NFTs, a member of the species associated with it will be taken care of
+        by one of our verified wildlife program partners with the aim of repopulating it. A
+        non-invasive ICARUS tracker will be placed on the chosen animal for data collection purposes.
+        The tracker will serve as a medium for researchers to understand the animal’s movements and
+        common behaviors in order to better protect it and enable it to thrive in the current
+        environment; it will also allow you to directly engage with the animal in the metaverse by
+        seeing and following its every move.
       </Typography>
       <Typography variant="h2">Future Plans</Typography>
-      <Typography variant="body1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius elit nec sem ultricies
-        porta. In ut rutrum enim. Fusce ex massa, imperdiet at fermentum id, faucibus non ante. Fusce
-        accumsan eget risus vel dapibus. Fusce hendrerit lobortis mi eget consequat. Integer id nisl
-        viverra, varius lacus at, bibendum velit. Maecenas nec luctus velit. Nullam venenatis dolor
-        orci, eget dictum nibh mollis quis. Aenean eu malesuada augue.
+      <Typography variant="body1" sx={{ margin: '0 auto', maxWidth: '80%' }}>
+        We are launching a metaverse for you to interact with your saved animals and travel to other
+        people's sanctuaries. Collect animals, Grow your animals, and Bond with your animals in an
+        immersive experience.
       </Typography>
       <Typography variant="h2">Contact Us</Typography>
-      <Typography variant="body1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat tempor molestie.
-        Curabitur scelerisque egestas massa, in euismod ex accumsan at. Interdum et malesuada fames ac
-        ante ipsum primis in faucibus. Mauris at turpis neque. Sed scelerisque erat ut lorem
-        scelerisque maximus. In imperdiet neque id diam dignissim, sed dictum arcu tristique. Interdum
-        et malesuada fames ac ante ipsum primis in faucibus.
+      <Typography variant="body1" sx={{ margin: '0 auto', maxWidth: '80%' }}>
+        Northeastern University Blockchain Club
       </Typography>
     </Container>
   );
